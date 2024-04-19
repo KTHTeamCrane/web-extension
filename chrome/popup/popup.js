7dcde2c81d0684ffee69adb351f7776d79319b75
Phanty133 <vintersk@gmail.com>
2024-04-19 11:53:57 +0200

    Updated extension

--- chrome/popup/popup.js
+++ chrome/popup/popup.js
@@ -5,9 +5,10 @@ const debugTextField = document.getElementById("big-red-button__debug-text")
 
 document.getElementById("big-red-button__button").onclick = async () => {
     console.log("Click")
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true})
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true })
     let expressServerRequestUrl = EXPRESS_API_DOMAIN + "/article/fake?article=" + tab.url
-    let expressResponse = await fetch(expressServerRequestUrl, {method: "GET"})
-    let html = await expressResponse.text()
-    debugTextField.innerHTML = html
+    let expressResponse = await fetch(expressServerRequestUrl, { method: "GET" })
+    let resp = await expressResponse.text()
+
+    debugTextField.innerHTML = resp
 }
