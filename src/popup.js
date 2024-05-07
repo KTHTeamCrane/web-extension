c07b03a9899b1d6a4277db14c953ed0c6ff13ae1
ohshitnotgood <98031796+ohshitnotgood@users.noreply.github.com>
2024-05-07 08:46:28 +0200

    LocalStorage implemented for popup

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,29 @@
 'use strict';
 
 import css from "./popup.css"
+
+const KEY_AUTO_DETECT = "toggle-auto-detect"
+const KEY_HIGHLIGHT = "toggle-enable-highlight"
+
+const toggleAutoDetect = document.getElementById(KEY_AUTO_DETECT)
+const toggleHighlight = document.getElementById(KEY_HIGHLIGHT)
+
+const stateAutoDetect = localStorage.getItem(KEY_AUTO_DETECT)
+const stateHighlight = localStorage.getItem(KEY_HIGHLIGHT)
+
+
+if (stateAutoDetect !=  null) {
+    toggleAutoDetect.checked = stateAutoDetect.toLowerCase() === 'true'
+}
+
+if (stateHighlight != null) {
+    toggleHighlight.checked = stateHighlight.toLowerCase() === 'true'
+}
+
+toggleAutoDetect.addEventListener("change", () => {
+    localStorage.setItem(KEY_AUTO_DETECT, toggleAutoDetect.checked.toString())
+})
+
+toggleHighlight.addEventListener("change", () => {
+    localStorage.setItem(KEY_HIGHLIGHT, toggleHighlight.checked.toString())
+})
